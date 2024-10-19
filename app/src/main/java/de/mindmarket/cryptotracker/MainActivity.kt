def95cd6660e377ae0e09443fbaf39cef46ee760
IlMike22 <michaelwidlok@gmail.com>
2024-10-19 08:11:08 +0200

    add coin list screen

--- app/src/main/java/de/mindmarket/cryptotracker/MainActivity.kt
+++ app/src/main/java/de/mindmarket/cryptotracker/MainActivity.kt
@@ -1,5 +1,6 @@
 package de.mindmarket.cryptotracker
 
+import CryptoTrackerTheme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,7 +12,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import de.mindmarket.cryptotracker.ui.theme.CryptoTrackerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/de/mindmarket/cryptotracker/crypto/presentation/coin_list/CoinListScreen.kt
@@ -0,0 +1,69 @@
+package de.mindmarket.cryptotracker.crypto.presentation.coin_list
+
+import CryptoTrackerTheme
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import de.mindmarket.cryptotracker.crypto.presentation.coin_list.components.CoinListItem
+import de.mindmarket.cryptotracker.crypto.presentation.coin_list.components.previewCoin
+import de.mindmarket.cryptotracker.crypto.presentation.models.CoinUi
+import de.mindmarket.cryptotracker.crypto.presentation.models.toCoinUi
+
+@Composable
+fun CoinListScreen(
+    state: CoinListState,
+    modifier: Modifier = Modifier
+) {
+    if (state.isLoading) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator()
+        }
+    } else {
+        LazyColumn(
+            modifier = modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(state.coins) { coinUi ->
+                CoinListItem(
+                    coinUi = coinUi,
+                    onClick = {},
+                    modifier = Modifier.fillMaxWidth()
+                )
+                HorizontalDivider()
+            }
+        }
+    }
+}
+
+@PreviewLightDark
+@Composable
+private fun CoinListPreview() {
+    CryptoTrackerTheme {
+        CoinListScreen(
+            state = CoinListState(
+                isLoading = false,
+                coins = (1..100).map {
+                    previewCoin.copy(id = it.toString()).toCoinUi()
+                }
+            ),
+            modifier = Modifier.background(MaterialTheme.colorScheme.background)
+        )
+    }
+}
